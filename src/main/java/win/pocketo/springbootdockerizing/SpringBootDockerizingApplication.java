b070956fee26405a909e5341a4cf68c23ab446cd
Vu Tong <tonghoangvu@outlook.com>
2023-05-12 16:33:28 +0700

    code: init project

---
+++ src/main/java/win/pocketo/springbootdockerizing/SpringBootDockerizingApplication.java
@@ -0,0 +1,13 @@
+package win.pocketo.springbootdockerizing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerizingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerizingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/win/pocketo/springbootdockerizing/SpringBootDockerizingApplicationTests.java
@@ -0,0 +1,13 @@
+package win.pocketo.springbootdockerizing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerizingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
