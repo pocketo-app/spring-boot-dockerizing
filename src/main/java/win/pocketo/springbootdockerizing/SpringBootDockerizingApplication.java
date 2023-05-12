1edc28e44f3959fb07145ec209a02a8f8ff7aa11
Vu Tong <tonghoangvu@outlook.com>
2023-05-12 16:56:00 +0700

    chore: setup the application

--- src/main/java/win/pocketo/springbootdockerizing/SpringBootDockerizingApplication.java
+++ src/main/java/win/pocketo/springbootdockerizing/SpringBootDockerizingApplication.java
@@ -2,12 +2,20 @@ package win.pocketo.springbootdockerizing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerizingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerizingApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello World";
 	}
 
 }
